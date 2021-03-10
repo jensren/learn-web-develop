54924899e14baa8d42819642b58ab52bc7ccdc22
Jenney <ren.jenney@gmail.com>
2021-03-09 20:16:18 -0500

    3-Pty APIs Ex3: starter

--- script.js
+++ script.js
@@ -1,138 +1,106 @@
-// Defining a baseURL and key to as part of the request URL
+// Get references to DOM elements we need to manipulate
+var searchTerm = document.querySelector('.search');
+var searchForm = document.querySelector('form');
+var submitBtn = document.querySelector('.submit');
 
-const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-const key = 'KYYACMXafxb3jJqsklWi3pjiAZrA3eag';
-let url;
+var section = document.querySelector('section');
 
-// Grab references to all the DOM elements you'll need to manipulate
+// When the window (tab) has finished loading, run onClientLoad() to get
+// It all started
+window.onload = onClientLoad;
 
-const searchTerm = document.querySelector('.search');
-const startDate = document.querySelector('.start-date');
-const endDate = document.querySelector('.end-date');
-const searchForm = document.querySelector('form');
-
-// This is never used
-// const submitBtn = document.querySelector('.submit');
-
-const nextBtn = document.querySelector('.next');
-const previousBtn = document.querySelector('.prev');
-
-const section = document.querySelector('section');
-const nav = document.querySelector('nav');
-
-// Hide the "Previous"/"Next" navigation to begin with, as we don't need it immediately
-nav.style.display = 'none';
-
-// define the initial page number and status of the navigation being displayed
-let pageNumber = 0;
-
-// This is never used
-// let displayNav = false;
-
-// Event listeners to control the functionality
-
-searchForm.addEventListener('submit', submitSearch);
-
-
-function submitSearch(e) {
-  pageNumber = 0;
-  fetchResults(e);
+// Load and inialize the API, then run the onYouTubeApiLoad() function once this is done
+function onClientLoad() {
+  gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
 
-function fetchResults(e) {
-  // Use preventDefault() to stop the form submitting
+// Attach your key to the API
+function onYouTubeApiLoad() {
+  // To get a key for your own application:
+  // 1. Go to https://console.cloud.google.com/apis/dashboard
+  // 2. Create a new project if you've not already got one
+  // 3. Click the Enable API button
+  // 4. Choose YouTube Data API
+  // 5. Click the Enable button
+  // 6. Click create credentials
+  // 7. Select "Web browser (JavaScript)" from the second dropdown
+  // 8. Click the "Public data" radio button
+  // 9. Click the "What credentials do I need?" button
+  // 10. Copy your API key and paste it in below
+  gapi.client.setApiKey('ENTER-API-KEY-HERE');
+
+  // Attach an event listener to the form so that a search is carried out
+  // when it is submitted — the search() function
+  searchForm.addEventListener('submit', search);
+}
+
+function search(e) {
+  // use preventDefault() to stop form actually submitting
   e.preventDefault();
 
-  // Assemble the full URL
-  url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchTerm.value + '&fq=document_type:("article")';
-
-  if (startDate.value !== '') {
-    url += '&begin_date=' + startDate.value;
-  };
-
-  if (endDate.value !== '') {
-    url += '&end_date=' + endDate.value;
-  };
-
-  // Use fetch() to make the request to the API
-  fetch(url).then(function (result) {
-    return result.json();
-  }).then(function (json) {
-    displayResults(json);
+  // create a search request using the Data API;
+  var request = gapi.client.youtube.search.list({
+    // set what kind of data the response will include
+    part: 'snippet',
+    // set the number of results that will be returned
+    maxResults: 10,
+    // set the search query to search for
+    q: searchTerm.value
   });
+
+  // send the request, and specify a function to run when the response returns
+  request.execute(onSearchResponse);
 }
 
-function displayResults(json) {
+// This function automatically has the response passed in as a parameter
+function onSearchResponse(response) {
+  // Empty the <section> element
   while (section.firstChild) {
-    section.removeChild(section.firstChild);
+      section.removeChild(section.firstChild);
   }
 
-  const articles = json.response.docs;
+  // Store the actual results of the search in a variable
+  var results = response.items;
 
-  if (articles.length === 10) {
-    nav.style.display = 'block';
-  } else {
-    nav.style.display = 'none';
+  // loop through the results and run displayVideo() on each
+  for(var i = 0; i < results.length; i++) {
+    displayVideo(results[i], i);
   }
+}
 
-  if (articles.length === 0) {
-    const para = document.createElement('p');
-    para.textContent = 'No results returned.'
-    section.appendChild(para);
-  } else {
-    for (var i = 0; i < articles.length; i++) {
-      const article = document.createElement('article');
-      const heading = document.createElement('h2');
-      const link = document.createElement('a');
-      const img = document.createElement('img');
-      const para1 = document.createElement('p');
-      const para2 = document.createElement('p');
-      const clearfix = document.createElement('div');
+function displayVideo(result, i) {
+  // Create a div with a unique ID for each video, and append it to the <section>
+  // The YouTube Iframe Player API will replace each one with
+  // an <iframe> containing the corresponding video
+  var vid = document.createElement('div');
+  vidId = 'vid' + i;
+  vid.id = vidId;
+  section.appendChild(vid);
 
-      let current = articles[i];
-      console.log(current);
+  // Use the YT.Player() constructor to create a new video player object,
+  // Specifying the ID of the element to be replaced by it (the <div>),
+  // A height and width, and an event handler to handle the custom onReady event
+  var player = new YT.Player(vidId, {
+    height: '360',
+    width: '480',
+    videoId: result.id.videoId,
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
 
-      link.href = current.web_url;
-      link.textContent = current.headline.main;
-      para1.textContent = current.snippet;
-      para2.textContent = 'Keywords: ';
-      for (let j = 0; j < current.keywords.length; j++) {
-        const span = document.createElement('span');
-        span.textContent += current.keywords[j].value + ' ';
-        para2.appendChild(span);
-      }
-
-      if (current.multimedia.length > 0) {
-        img.src = 'http://www.nytimes.com/' + current.multimedia[0].url;
-        img.alt = current.headline.main;
-      }
-
-      clearfix.setAttribute('class', 'clearfix');
-
-      article.appendChild(heading);
-      heading.appendChild(link);
-      article.appendChild(img);
-      article.appendChild(para1);
-      article.appendChild(para2);
-      article.appendChild(clearfix);
-      section.appendChild(article);
+  // The onPlayerReady() handler grabs the ID of each video, and checks its duration
+  // If the duration is 0, the video can't be played, so we just delete it
+  function onPlayerReady(e) {
+    var myId = e.target.a.id;
+    var duration = e.target.getDuration();
+    if(duration === 0) {
+      console.log('Video ' + myId + ' cannot be played, so it was deleted.');
+      section.removeChild(e.target.a);
+    } else {
+      var myId = e.target.a.id;
+      console.log('Video ' + myId + ' ready to play.');
     }
   }
 }
-
-nextBtn.addEventListener('click', nextPage);
-previousBtn.addEventListener('click', previousPage);
-
-function nextPage(e) {
-  pageNumber++;
-  fetchResults(e);
-};
-
-function previousPage(e) {
-  if (pageNumber > 0) {
-    pageNumber--;
-  } else {
-    return;
-  }
-  fetchResults(e);
-};
+</script>
